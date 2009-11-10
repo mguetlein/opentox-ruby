331d50f69af8f8b8df7f600d3d82533811344bb0
Christoph Helma <helma@in-silico.de>
2009-11-10 15:16:49 +0100

    Installation instructions for openbabel ruby bindings

--- lib/opentox-ruby-api-wrapper.rb
+++ lib/opentox-ruby-api-wrapper.rb
@@ -1,5 +1,11 @@
-['rubygems', 'sinatra', 'sinatra/url_for',  'redis','builder', 'rest_client', 'yaml', 'cgi', 'openbabel', 'spork', 'environment'].each do |lib|
+['rubygems', 'sinatra', 'sinatra/url_for',  'redis','builder', 'rest_client', 'yaml', 'cgi', 'spork', 'environment'].each do |lib|
 	require lib
+end
+
+begin
+	require 'openbabel'
+rescue
+	puts "Please install Openbabel with 'rake openbabel:install' in the compound component"
 end
 
 ['opentox', 'compound','feature','dataset','algorithm','model','task','utils'].each do |lib|
