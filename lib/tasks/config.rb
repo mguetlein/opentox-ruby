70fef6f3fbf3e629947534949774d93dc5ea7fe2
Christoph Helma <helma@in-silico.de>
2010-02-01 16:19:13 +0100

    central configuration for config.ru added

---
+++ lib/tasks/config.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'rack'
+require 'rack/contrib'
+require 'application.rb'
+
+# log at centralized place
+logfile = "#{LOG_DIR}/#{ENV["RACK_ENV"]}.log"
+log = File.new(logfile, "a+")
+$stdout.reopen(log)
+$stderr.reopen(log)
+$stdout.sync = true
+$stderr.sync = true
+set :raise_errors, true 
+
+['public','tmp'].each do |dir|
+	FileUtils.mkdir_p dir unless File.exists?(dir)
+end
+ 
+use Rack::ShowExceptions
