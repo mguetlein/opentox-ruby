87fa8c2c74484d85da3f581929a603f31ecc92d0
mr <mr@mrautenberg.de>
2011-02-09 17:12:51 +0100

    task with subtask from mguetlein

--- lib/task.rb
+++ lib/task.rb
@@ -107,20 +107,9 @@ module OpenTox
 
     
     def self.from_rdfxml(rdfxml)
-      file = Tempfile.new("ot-rdfxml")
-      file.puts rdfxml
-      file.close
-      file = "file://"+file.path
-       
-      # PENDING
-      raise "Parse from file not working: what is the base-object-uri??? (omitted in triples)"    
-          
-      parser = Parser::Owl::Generic.new file
-      metadata = parser.load_metadata
-      puts metadata.inspect
-      
-      task = Task.new(uri)
-      task.add_metadata(metadata)
+      owl = OpenTox::Parser::Owl.from_rdf(rdfxml, OT.Task)
+      task = Task.new(owl.uri)
+      task.add_metadata(owl.metadata)
       task
     end
 
@@ -149,7 +138,7 @@ module OpenTox
     end
     
     def cancel
-      RestClientWrapper.put(File.join(@uri,'Cancelled'))
+      RestClientWrapper.put(File.join(@uri,'Cancelled'),{:cannot_be => "empty"})
       load_metadata
     end
 
@@ -247,6 +236,7 @@ module OpenTox
     # @param [optional,Numeric] dur seconds pausing before cheking again for completion
     def wait_for_completion( waiting_task=nil, dur=0.3)
       
+      waiting_task.waiting_for(self.uri) if waiting_task
       due_to_time = Time.new + DEFAULT_TASK_MAX_DURATION
       LOGGER.debug "start waiting for task "+@uri.to_s+" at: "+Time.new.to_s+", waiting at least until "+due_to_time.to_s
       
@@ -262,7 +252,7 @@ module OpenTox
           raise "max wait time exceeded ("+DEFAULT_TASK_MAX_DURATION.to_s+"sec), task: '"+@uri.to_s+"'"
         end
       end
-      
+      waiting_task.waiting_for(nil) if waiting_task
       LOGGER.debug "Task '"+@metadata[OT.hasStatus].to_s+"': "+@uri.to_s+", Result: "+@metadata[OT.resultURI].to_s
     end
     
@@ -276,6 +266,10 @@ module OpenTox
         RestClientWrapper.put(File.join(@uri,'Running'),{:percentageCompleted => pct})
         load_metadata
       end
+    end
+    
+    def waiting_for(task_uri)
+      RestClientWrapper.put(File.join(@uri,'Running'),{:waiting_for => task_uri})
     end
     
     private
@@ -330,6 +324,10 @@ module OpenTox
       else
         nil
       end
+    end
+    
+    def waiting_for(task_uri)
+      @task.waiting_for(task_uri)
     end
     
     def progress(pct)
